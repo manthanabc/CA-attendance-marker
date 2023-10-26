9c7d75d59a545522432bdb941542d1b5b2d7cf3c
0xFFF42 <manthanmji@gmail.com>
2023-10-26 21:52:19 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const {Builder, Browser, By, Key, until, Select} = require('selenium-webdriver');
+
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+(async function example() {
+  let driver = await new Builder().forBrowser(Browser.FIREFOX).build();
+  try {
+   let numbers = [68, 73, 74, 75];
+   for(const i of numbers) {
+             // i=74;
+    await driver.get('https://docs.google.com/forms/d/e/1FAIpQLSf0bouK1DVQdXWp8lPprbd9JyZiYlJtWMFqJx-kGEsThaGx6A/viewform');
+    await driver.wait(until.titleIs('Attendance'), 6000);
+    await sleep(1000);
+
+   await driver.actions()
+          .sendKeys(Key.TAB, Key.TAB, Key.TAB)
+          .perform()
+   await driver.actions()
+          .sendKeys(Key.ARROW_DOWN, Key.ARROW_DOWN, Key.ARROW_DOWN)
+          .perform()
+    await sleep(50);
+   await driver.actions()
+          .sendKeys(Key.ARROW_DOWN)
+          .perform()
+    await sleep(50);
+   await driver.actions()
+          .sendKeys(Key.SPACE)
+          .perform()
+    await sleep(500)
+    await driver.actions().sendKeys(Key.TAB).perform()
+    await sleep(50)
+    await driver.actions().sendKeys(i.toString()).perform()
+    await sleep(50)
+    await driver.actions().sendKeys(Key.RETURN).perform();
+    await sleep(50)
+    await driver.actions().sendKeys(Key.TAB).perform();
+    await sleep(50)
+    await driver.actions().sendKeys(Key.RETURN).perform();
+    console.log("marked ", i);
+    }
+    await sleep(500)
+  } finally {
+    await driver.quit();
+  }
+})();
