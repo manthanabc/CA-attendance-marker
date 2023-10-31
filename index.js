72626d7df9b00f25cf80789ddabd2f8f932fcdd2
0xFFF42 <manthanmji@gmail.com>
2023-11-01 00:35:03 +0530

    changed something

--- index.js
+++ index.js
@@ -1,28 +1,35 @@
 const {Builder, Browser, By, Key, until, Select} = require('selenium-webdriver');
 
+const express = require("express");
+const app = express()
+const port = 3000
+
+
+
+app.get('/mark',  async (req, res) => {
+ res.send('Hello World!')
 const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
-(async function example() {
+// (async function example() {
   let driver = await new Builder().forBrowser(Browser.FIREFOX).build();
   try {
-   let numbers = [68, 73, 74, 75];
+   let numbers = [73, 74, 75];
    for(const i of numbers) {
-             // i=74;
     await driver.get('https://docs.google.com/forms/d/e/1FAIpQLSf0bouK1DVQdXWp8lPprbd9JyZiYlJtWMFqJx-kGEsThaGx6A/viewform');
     await driver.wait(until.titleIs('Attendance'), 6000);
     await sleep(1000);
 
-   await driver.actions()
+    await driver.actions()
           .sendKeys(Key.TAB, Key.TAB, Key.TAB)
           .perform()
-   await driver.actions()
+    await driver.actions()
           .sendKeys(Key.ARROW_DOWN, Key.ARROW_DOWN, Key.ARROW_DOWN)
           .perform()
     await sleep(50);
-   await driver.actions()
+    await driver.actions()
           .sendKeys(Key.ARROW_DOWN)
           .perform()
     await sleep(50);
-   await driver.actions()
+    await driver.actions()
           .sendKeys(Key.SPACE)
           .perform()
     await sleep(500)
@@ -41,4 +48,8 @@ const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
   } finally {
     await driver.quit();
   }
-})();
+// })();
+})
+app.listen(port, () => {
+ console.log(`Example app listening on port ${port}`)
+})
